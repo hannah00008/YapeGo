c1f73b26ee1a2c28279f565b9fe3784dd83b1d87
Hannah <hannamelendez08@gmail.com>
2026-04-11 23:22:47 -0500

    cambios

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,19 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore  } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCxFZ-4zM1XQqkBCBGn8QWydVOMSwBcfrU",
-  authDomain: "mini-proyecto-d4d04.firebaseapp.com",
-  projectId: "mini-proyecto-d4d04",
-  storageBucket: "mini-proyecto-d4d04.firebasestorage.app",
-  messagingSenderId: "199649537471",
-  appId: "1:199649537471:web:a701864fce645c3931c50a"
+  apiKey: "AIzaSyBrqsiYBK4-V9c8zKm1ERVRD-_qiSOtXSs",
+  authDomain: "yapego-31eb7.firebaseapp.com",
+  projectId: "yapego-31eb7",
+  storageBucket: "yapego-31eb7.firebasestorage.app",
+  messagingSenderId: "600630157964",
+  appId: "1:600630157964:web:eab4be25b80ada8f1c91a9", 
+  measurementId: "G-61476DN93R"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+console.log(app)
 export const db = getFirestore(app);
 export const auth = getAuth(app);
